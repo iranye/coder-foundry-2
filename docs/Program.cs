c26502c0f33a0daf4b0672053f230272904b6786
Ira Nye <inye@hchb.com>
2022-01-11 16:34:21 -0500

    Additional Fix for numbered output to support (bad 3x padding output)
    Add sln & proj to hold html, css, etc

---
+++ docs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace coder_foundry_2_cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
