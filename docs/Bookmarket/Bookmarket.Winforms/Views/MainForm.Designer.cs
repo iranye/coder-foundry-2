189789e4315dcd98adf2ea824f76eb808f218717
Ira Nye <iranye@gmail.com>
2023-01-22 19:06:41 -0500

    Add more controls

--- docs/Bookmarket/Bookmarket.Winforms/Views/MainForm.Designer.cs
+++ docs/Bookmarket/Bookmarket.Winforms/Views/MainForm.Designer.cs
@@ -34,75 +34,112 @@
             this.label2 = new System.Windows.Forms.Label();
             this.btnImportHtml = new System.Windows.Forms.Button();
             this.btnImportJson = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.clbTags = new System.Windows.Forms.CheckedListBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtImport
             // 
-            this.txtImport.Location = new System.Drawing.Point(176, 32);
+            this.txtImport.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtImport.Location = new System.Drawing.Point(154, 24);
+            this.txtImport.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtImport.Multiline = true;
             this.txtImport.Name = "txtImport";
-            this.txtImport.Size = new System.Drawing.Size(612, 265);
+            this.txtImport.Size = new System.Drawing.Size(536, 200);
             this.txtImport.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(176, 9);
+            this.label1.Location = new System.Drawing.Point(154, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.Size = new System.Drawing.Size(59, 15);
             this.label1.TabIndex = 1;
             this.label1.Text = "Paste Text";
             // 
             // txtOutput
             // 
-            this.txtOutput.Location = new System.Drawing.Point(176, 459);
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(154, 344);
+            this.txtOutput.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtOutput.Multiline = true;
             this.txtOutput.Name = "txtOutput";
-            this.txtOutput.Size = new System.Drawing.Size(612, 265);
+            this.txtOutput.Size = new System.Drawing.Size(536, 200);
             this.txtOutput.TabIndex = 2;
             // 
             // label2
             // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(176, 436);
+            this.label2.Location = new System.Drawing.Point(154, 327);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.Size = new System.Drawing.Size(45, 15);
             this.label2.TabIndex = 3;
             this.label2.Text = "Output";
             // 
             // btnImportHtml
             // 
-            this.btnImportHtml.Location = new System.Drawing.Point(176, 303);
+            this.btnImportHtml.Location = new System.Drawing.Point(154, 227);
+            this.btnImportHtml.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnImportHtml.Name = "btnImportHtml";
-            this.btnImportHtml.Size = new System.Drawing.Size(110, 29);
+            this.btnImportHtml.Size = new System.Drawing.Size(96, 22);
             this.btnImportHtml.TabIndex = 4;
             this.btnImportHtml.Text = "Import Html";
             this.btnImportHtml.UseVisualStyleBackColor = true;
-            this.btnImportHtml.Click += new System.EventHandler(this.btnImportHtml_Click);
+            this.btnImportHtml.Click += new System.EventHandler(this.btnImportJson_Click);
             // 
             // btnImportJson
             // 
-            this.btnImportJson.Location = new System.Drawing.Point(292, 303);
+            this.btnImportJson.Location = new System.Drawing.Point(256, 227);
+            this.btnImportJson.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnImportJson.Name = "btnImportJson";
-            this.btnImportJson.Size = new System.Drawing.Size(110, 29);
+            this.btnImportJson.Size = new System.Drawing.Size(96, 22);
             this.btnImportJson.TabIndex = 5;
             this.btnImportJson.Text = "Import JSON";
             this.btnImportJson.UseVisualStyleBackColor = true;
-            this.btnImportHtml.Click += new System.EventHandler(this.btnImportJson_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.clbTags);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(147, 552);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tags";
+            // 
+            // clbTags
+            // 
+            this.clbTags.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.clbTags.FormattingEnabled = true;
+            this.clbTags.Location = new System.Drawing.Point(3, 19);
+            this.clbTags.Name = "clbTags";
+            this.clbTags.Size = new System.Drawing.Size(141, 526);
+            this.clbTags.TabIndex = 0;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 736);
+            this.ClientSize = new System.Drawing.Size(700, 552);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnImportJson);
             this.Controls.Add(this.btnImportHtml);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtOutput);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtImport);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MainForm";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,5 +153,7 @@
         private Label label2;
         private Button btnImportHtml;
         private Button btnImportJson;
+        private GroupBox groupBox1;
+        private CheckedListBox clbTags;
     }
 }
