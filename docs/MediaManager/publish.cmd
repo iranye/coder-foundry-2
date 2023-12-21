94587fdb1b960eccb5646a30ece2dba99ee10f91
Ira Nye <iranye@gmail.com>
2023-12-21 15:49:27 -0500

    cleanup publish script

--- docs/MediaManager/publish.cmd
+++ docs/MediaManager/publish.cmd
@@ -1,8 +1,7 @@
 @echo off
 cd
 set srcdir=MediaManager.WPF
-set pubdir=..\..\mediamanager-pub\Release
-set zipdir=..\..\mediamanager-pub\MediaManager.pub
+set pubdir=..\..\mediamanager-pub\MediaManager.pub
 dotnet cake --target="Version"
 
 type Directory.Build.props
@@ -10,16 +9,11 @@ cd %srcdir%
 echo publishing %srcdir% to %pubdir%...
 pause
 
-:: dotnet publish -o C:\src\NewStuff\Pluralsight\Thomas\WPF6-Fundamentals\WPF6-Fundamentals\WorkTool.WPF\pub\Release
 dotnet publish -o %pubdir%
 
 pause
 
-echo robocopy %pubdir% %zipdir% /mir /tee /v
-pause
-robocopy %pubdir% %zipdir% /mir /tee /v
-
-echo 7zproject %zipdir% ..
-7zproject %zipdir% %zipdir%\..
+echo 7zproject %pubdir% %pubdir%\..
+7zproject %pubdir% %pubdir%\..
 
 cd ..
